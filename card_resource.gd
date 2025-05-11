579be8b0092a6dfc30607fe5627890862a55972d
Valerian Backeberg <valerian@backebergs.de>
2025-05-11 19:45:10 +0200

    init

---
+++ card_resource.gd
@@ -0,0 +1,15 @@
+extends Sprite2D
+
+var value: int
+
+func _init(v: int) -> void:
+	value = v
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ players_hand.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+var cards: Array
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	cards = []
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
