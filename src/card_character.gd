3fb6cd80475a63ea13a7a2035b1a18251a1a8782
Valerian Backeberg <valerian@backebergs.de>
2025-08-03 10:20:03 +0200

    Character: add backside property and load back or front texture accordingly

--- src/card_character.gd
+++ src/card_character.gd
@@ -9,16 +9,18 @@ var points: int
 var diamonds: int
 var effect := func(_player): pass
 var asset_path: String
+var backside := false
 
-func _init(p_buy, p_points, p_diamonds, p_asset) -> void:
+func _init(p_buy, p_points, p_diamonds, p_asset_path) -> void:
 	buy = p_buy
 	points = p_points
 	diamonds = p_diamonds
-	asset_path = "res://assets/characters/" + p_asset + ".png"
+	asset_path = "res://assets/characters/" + p_asset_path + ".png"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	texture_normal = load(asset_path)
+	var p = "res://assets/character_back.png" if backside else asset_path
+	texture_normal = load(p)
 	scale = Vector2(128.0, 200.0) / texture_normal.get_size()
 	pass # Replace with function body.
 
