70d47e40e67f73baa54d6f41a6ede5f0851021b7
Valerian Backeberg <valerian@backebergs.de>
2025-08-10 15:27:03 +0200

    test tryout

---
+++ test/test_middle_area.gd
@@ -0,0 +1,16 @@
+#!/usr/bin/env -S godot -s
+extends SceneTree
+
+func _init():
+	print("Hello!")
+	# _is_n_of_a_kind_returns_paid_if_match_exactly()
+	quit()
+
+
+# func _is_n_of_a_kind_returns_paid_if_match_exactly():
+	
+	# var MiddleArea = preload("res://src/middle_area.gd")
+	# var uut = MiddleArea.new()
+	# uut._is_n_of_a_kind()
+
+	
