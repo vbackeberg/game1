c39e2b96329dbfc420ea531ca5a9cb4d8b24178a
Valerian Backeberg <valerian@backebergs.de>
2025-11-15 16:33:19 +0100

    add test scene and script that can launch specific scenarios

--- test/test.gd
+++ test/test.gd
@@ -2,7 +2,8 @@ extends Node
 
 func _ready() -> void:
 	var player = $Main.players[0]
-	var card = $Main/MiddleArea.characterCards.find_custom(is_dog.bind())
+	var idx = MiddleArea.characterCards.find_custom(is_dog.bind())
+	var card = MiddleArea.characterCards.pop_at(idx)
 	player.add_character(card)
 
 	
