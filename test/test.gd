2f63850130f301d6c4c8b283eda37bdc41ed9dcb
Valerian Backeberg <valerian@backebergs.de>
2025-12-01 16:05:25 +0100

    test use game mgr

--- test/test.gd
+++ test/test.gd
@@ -2,8 +2,8 @@ extends Node
 
 func _ready() -> void:
 	var player = $Main.players[0]
-	var idx = MiddleArea.characterCards.find_custom(is_dog.bind())
-	var card = MiddleArea.characterCards.pop_at(idx)
+	var idx = GameManager.characterCards.find_custom(is_dog.bind())
+	var card = GameManager.characterCards.pop_at(idx)
 	player.add_character(card)
 
 	
