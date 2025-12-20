5ff64641c10dc0e7e24fbd33fc6224f671da68fc
Valerian Backeberg <valerian@backebergs.de>
2025-12-20 13:42:22 +0100

    Test scene: give characters and resources

--- test/test.gd
+++ test/test.gd
@@ -1,11 +1,21 @@
 extends Node
 
+var player: PlayerArea
+
 func _ready() -> void:
-	var player = $Main.players[0]
-	var idx = GameManager.characterCards.find_custom(is_dog.bind())
+	player = $Main.players[0]
+	give_player_character("actions-1-3-5-7")
+	give_player_resource(1)
+	give_player_resource(3)
+	give_player_resource(5)
+	give_player_resource(7)
+	
+
+func give_player_character(cardName: String) -> void:
+	var idx = GameManager.characterCards.find_custom(func(c): return c.asset_path == "res://assets/characters/" + cardName + ".png")
 	var card = GameManager.characterCards.pop_at(idx)
 	player.add_character(card)
 
-	
-func is_dog(card: CardCharacter):
-	return card.asset_path == "res://assets/characters/dog-1-1-1-1.png"
+func give_player_resource(value: int) -> void:
+	GameManager.resourceCards.remove_at(GameManager.resourceCards.find(value))
+	player.add_resource(value)
