6e1debf49c561230969cc6dd978fcfeec35d9ced
Valerian <valerian@backebergs.de>
2025-12-31 11:23:32 +0100

    fix return resources

--- src/characters/discard_7.gd
+++ src/characters/discard_7.gd
@@ -6,8 +6,11 @@ func _init():
 	asset_path = "res://assets/characters/discard-7.png"
 
 func buy(player: PlayerArea) -> Variant:
-	if _is_owner(player):
-		var paid = _sums_up_to_s_using_exactly_n(player, 7, 3)
-		if paid:
-			return paid
+	if not _is_owner(player):
+		return null
+
+	var resources = _sums_up_to_s_using_exactly_n(player, 7, 3)
+	if not resources:
+		return null
+
 	return null
