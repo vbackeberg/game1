ffb684d464cdb23adff102811cd765fdd5ebf2d8
Valerian Backeberg <valerian@backebergs.de>
2026-01-06 13:46:15 +0100

    test correct character find

--- test/test.gd
+++ test/test.gd
@@ -4,15 +4,15 @@ var player: PlayerArea
 
 func _ready() -> void:
 	player = $Main.players[0]
-	give_player_character("actions-1-3-5-7")
-	give_player_resource(1)
+	give_player_character("three_for_wildcard")
 	give_player_resource(3)
-	give_player_resource(5)
-	give_player_resource(7)
+	give_player_resource(4)
+	give_player_resource(3)
+	give_player_resource(3)
 	
 
 func give_player_character(cardName: String) -> void:
-	var idx = GameManager.characterCards.find_custom(func(c): return c.resource_path == "res://assets/characters/" + cardName + ".png")
+	var idx = GameManager.characterCards.find_custom(func(c): return c.resource_path == "res://src/characters/" + cardName + ".tscn")
 	if idx == -1:
 		print("Test setup failed! Character card not found: " + cardName)
 	var card = GameManager.characterCards.pop_at(idx).instantiate()
