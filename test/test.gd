5da56a527e14937469a488b8ff4947b6738ee79d
Valerian <valerian@backebergs.de>
2026-01-19 11:27:24 +0100

    test file

--- test/test.gd
+++ test/test.gd
@@ -4,7 +4,9 @@ var player: PlayerArea
 
 func _ready() -> void:
 	player = $Main.players[0]
-	give_player_character("actions-1-3-5-7")
+	give_player_character("actions_1_3_5_7")
+	give_player_character("dwarf_1")
+	give_player_resource(1)
 	give_player_resource(1)
 	give_player_resource(3)
 	give_player_resource(5)
@@ -12,8 +14,8 @@ func _ready() -> void:
 	
 
 func give_player_character(cardName: String) -> void:
-	var idx = GameManager.characterCards.find_custom(func(c): return c.asset_path == "res://assets/characters/" + cardName + ".png")
-	var card = GameManager.characterCards.pop_at(idx)
+	var idx = GameManager.characterCards.find_custom(func(c): return c.resource_path == "res://src/characters/" + cardName + ".tscn")
+	var card = GameManager.characterCards.pop_at(idx).instantiate()
 	player.add_character(card)
 
 func give_player_resource(value: int) -> void:
