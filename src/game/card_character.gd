f922d23f7d929565bbe05bef564ea3002fadbfee
Valerian Backeberg <valerian@backebergs.de>
2026-02-21 14:24:20 +0100

    move game scenes into game folder

---
+++ src/game/card_character.gd
@@ -0,0 +1,145 @@
+class_name CardCharacter
+extends TextureButton
+
+var slot: int
+var points: int
+var playerOwner: PlayerArea
+var scene:= preload("res://src/game/card_character.tscn")
+
+## Override to set buy conditions
+func buy(_player: PlayerArea) -> bool:
+	return false
+
+func select(forDiscard := false):
+	self.modulate = Color(1.2, 0.8, 0.8) if forDiscard else Color(1.2, 1.2, 0.8) # Yellow and Red tint
+
+func deselect():
+	self.modulate = Color(1, 1, 1) # Reset to normal color
+
+## Override to set a permanent effect
+func activate_permanent_effect():
+	pass
+
+func _is_owner(player: PlayerArea):
+	return player == playerOwner
+
+static func concat(arr: Array) -> String:
+	var result = ""
+	for num in arr:
+		result += str(num)
+	return result
+
+# Buy functions
+
+static func _sums_up_to_s_using_exactly_n(player: PlayerArea, s: int, n: int) -> bool:
+	var resources = []
+	resources.append_array(player.selectedResources)
+	resources.append_array(player.selectedVirtualResources)
+
+	while resources.size() > 0:
+		var card = resources.pop_back()
+		s -= card.resourceValue
+		n -= 1
+
+	if n != 0 or s != 0:
+		return false
+
+	return true
+
+static func _sums_up_to_s(player: PlayerArea, s: int) -> bool:
+	var resources = []
+	resources.append_array(player.selectedResources)
+	resources.append_array(player.selectedVirtualResources)
+
+	while resources.size() > 0:
+		var card = resources.pop_back()
+		s -= card.resourceValue
+	
+	if s != 0:
+		return false
+	
+	return true
+
+static func _n_resources_selected(player: PlayerArea, n: int):
+	return player.selectedVirtualResources.size() + player.selectedResources.size() == n
+
+static func _is_three_odd_or_even(player: PlayerArea, should_be_even: bool) -> bool:
+	var resources = []
+	resources.append_array(player.selectedResources)
+	resources.append_array(player.selectedVirtualResources)
+
+	var n = 3
+
+	while resources.size() > 0:
+		var card = resources.pop_back()
+		if (card.resourceValue % 2 == 0) == should_be_even:
+			n -= 1
+		else:
+			return false
+
+	if n != 0:
+		return false
+	return true
+
+static func _is_street_of_n(player: PlayerArea, n: int) -> bool:
+	var resources = []
+	resources.append_array(player.selectedResources)
+	resources.append_array(player.selectedVirtualResources)
+
+	if resources.size() != n:
+		return false
+
+	resources.sort_custom(func(a, b): return a.resourceValue - b.resourceValue)
+	
+	for i in range(1, n):
+		if resources[i].resourceValue != resources[i - 1].resourceValue + 1:
+			return false
+
+	return true
+
+static func _has_selected(player: PlayerArea, given: Array[int]) -> bool:
+	var resources = []
+	resources.append_array(player.selectedResources.duplicate())
+	resources.append_array(player.selectedVirtualResources.duplicate())
+	var indices = _find(resources, given)
+	return indices.size() != 0
+
+static func _has_n_of_same_kind(player: PlayerArea, n: int) -> bool:
+	var resources = []
+	resources.append_array(player.selectedResources.duplicate())
+	resources.append_array(player.selectedVirtualResources.duplicate())
+	var indices = _find_n_of_same_kind(resources, n)
+	return indices.size() != 0
+
+## Returns the indices of the found resources or an empty array if not all found
+static func _find(resources: Array, given: Array[int]) -> Array:
+	var indices = []
+
+	for g in given:
+		var idx = resources.find_custom(func(r): return r.resourceValue == g)
+		if idx != -1:
+			resources.remove_at(idx)
+			indices.append(idx)
+		else:
+			return []
+	return indices
+
+static func _find_n_of_same_kind(resources: Array, n: int) -> Array:
+	for i in range(1, 8):
+		var g = []
+		g.resize(n)
+		g.fill(i)
+		var indices = _find(resources, g)
+		if indices.size() == n:
+			return indices
+	return []
+
+static func _has_either_or(player: PlayerArea, eitherV: Array[int], orV: Array[int]) -> bool:
+	var resources = []
+	resources.append_array(player.selectedResources.duplicate())
+	resources.append_array(player.selectedVirtualResources.duplicate())
+
+	if _find(resources, eitherV).size() != 0 or _find(resources, orV).size() != 0:
+		return true
+
+	return false
---
+++ src/game/player_area.gd
@@ -0,0 +1,235 @@
+class_name PlayerArea
+extends Node2D
+
+const CARD_WIDTH = 128.0
+var playerName: String
+var resourcesOnHand: Array[CardResource]
+var resourceCapacity: int
+var charactersOnPayField: Array[CardCharacter]
+var diamonds: Array[CardCharacter]
+var selectedVirtualResources: Array[CardCharacter]
+var selectedResources: Array[CardResource]
+var selectedDiamonds: Array[CardCharacter]
+var charactersPlayed: Array[CardCharacter]
+var actionsThisTurn: int
+var actionsPerTurn: int
+var actionsUsed: int
+var discardMode: bool
+var numToDiscard: int
+
+var victoryPoints: int
+
+signal action_used()
+signal discard_started()
+signal discard_finished()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	playerName = str(randi() % 100 + 1)
+	resourcesOnHand = []
+	resourceCapacity = 5
+	charactersOnPayField = []
+	diamonds = []
+	selectedVirtualResources = []
+	selectedResources = []
+	selectedDiamonds = []
+	charactersPlayed = []
+	visible = false
+	actionsPerTurn = 3
+	actionsThisTurn = actionsPerTurn
+	actionsUsed = 0
+	victoryPoints = 0
+
+	discardMode = false
+	numToDiscard = 0
+	$ConfirmDiscardButton.visible = false
+
+## Appends card
+func add_resource(value: int):
+	var card_node = load("res://src/game/card_resource.tscn").instantiate() as CardResource
+	card_node.custom_minimum_size = Vector2(CARD_WIDTH, 200.0)
+	card_node.texture_normal = load("res://assets/resource" + str(value) + ".png")
+	card_node.resourceValue = value
+	card_node.visible = visible # Match parent's visibility
+	resourcesOnHand.append(card_node)
+	add_child(card_node)
+	
+	var card_index = resourcesOnHand.size() - 1
+	card_node.position.x = 24.0 + card_index * (CARD_WIDTH + 24.0)
+	card_node.position.y = get_viewport().size.y / 2
+	
+	card_node.pressed.connect(_on_resource_card_pressed.bind(card_node))
+
+func discard_if_too_many_cards():
+	var excess = resourcesOnHand.size() - resourceCapacity
+	if excess > 0:
+		discard_started.emit()
+		discardMode = true
+		numToDiscard = excess
+		selectedResources.clear()
+		print("You have " + str(numToDiscard) + " cards to discard.")
+	else:
+		discard_finished.emit()
+
+## Selects or deselects a resource on press
+func _on_resource_card_pressed(card: CardResource) -> void:
+	var index = selectedResources.find(card)
+	if discardMode:
+		if index == -1:
+			if selectedResources.size() == numToDiscard:
+				return
+
+			selectedResources.append(card)
+			card.select(true)
+		else:
+			selectedResources.remove_at(index)
+			card.deselect()
+		
+		if selectedResources.size() == numToDiscard:
+			$ConfirmDiscardButton.visible = true
+		else:
+			$ConfirmDiscardButton.visible = false
+
+	else:
+		if index == -1:
+			selectedResources.append(card)
+			card.select()
+		else:
+			selectedResources.remove_at(index)
+			card.deselect()
+
+func _on_confirm_discard_button_pressed() -> void:
+	for r in selectedResources:
+		GameManager.graveyardResources.append(r)
+		resourcesOnHand.erase(r)
+		r.queue_free()
+		numToDiscard -= 1
+		if numToDiscard == 0:
+			$ConfirmDiscardButton.visible = false
+			discardMode = false
+			selectedResources.clear()
+			reorder_resource_cards()
+			discard_finished.emit()
+
+## Adds a character card with given specs and puts it on the right side.
+func add_character(card: CardCharacter):
+	add_child(card)
+	card.visible = visible
+	card.playerOwner = self
+	charactersOnPayField.append(card)
+
+	var card_index = charactersOnPayField.size()
+	card.position.x = get_viewport().size.x - 24.0 - card_index * (CARD_WIDTH + 24.0)
+	card.position.y = get_viewport().size.y / 2
+	
+	card.pressed.connect(_on_unplayed_character_card_pressed.bind(card))
+
+## If buy successful, puts selected resources to graveyard, replaces character card, updates victory points and emits action used.
+func _on_unplayed_character_card_pressed(card: CardCharacter) -> void:
+	print("Selected: Resources: " + ",".join(selectedResources.map(func(r): return r.resourceValue)) + ", Diamonds: " + ",".join(selectedDiamonds) + ", Virtual Resources: " + ",".join(selectedVirtualResources.map(func(r): return r.resourceValue)))
+	var success = card.buy.call(self)
+	if not success:
+		print("Selected resources are not correct.")
+		# TODO show label with missing resources	
+
+		for r in selectedResources:
+			r.deselect()
+
+		for d in selectedDiamonds:
+			d.deselect()
+	else:
+		for r in selectedResources:
+			GameManager.graveyardResources.append(r.resourceValue)
+			resourcesOnHand.erase(r)
+			r.queue_free()
+			
+		reorder_resource_cards()
+		
+		for d in selectedDiamonds:
+			GameManager.graveyardCharacters.append(d)
+			diamonds.erase(d)
+			d.queue_free()
+
+		_reorder_diamonds()
+
+		_place_character_on_played_area(card)
+		
+		victoryPoints += card.points
+		action_used.emit()
+
+	for vr in selectedVirtualResources:
+		vr.get_node("ActivatedOverlay").visible = false
+		
+	selectedResources.clear()
+	selectedDiamonds.clear()
+	selectedVirtualResources.clear()
+	character_bought.emit()
+
+signal character_bought()
+
+## Places a character with its backside up.
+## Note: Keeping track of the character is important in case it's reshuffled into the stack from the graveyard, later.
+func add_diamond(card: CardCharacter):
+	card.backside = true
+	card.visible = visible
+	diamonds.append(card)
+	add_child(card)
+
+	var card_index = diamonds.size()
+	card.position.x = get_viewport().size.x - 24.0 - card_index * (CARD_WIDTH + 24.0)
+	card.position.y = 24.0
+	
+	card.pressed.connect(_on_diamond_card_pressed.bind(card))
+
+
+func _on_diamond_card_pressed(card: CardCharacter) -> void:
+	var index = selectedDiamonds.find(card)
+
+	if index == -1:
+		selectedDiamonds.append(card)
+		card.select()
+	else:
+		selectedDiamonds.remove_at(index)
+		card.deselect()
+
+
+func _place_character_on_played_area(card: CardCharacter):
+	charactersPlayed.append(card)
+	var card_index = charactersPlayed.size()
+	card.position.x = 24.0 + card_index * (CARD_WIDTH + 24.0)
+	card.position.y = 24.0 + 200.0
+	card.rotation_degrees = 180
+	card.pressed.disconnect(_on_unplayed_character_card_pressed)
+	card.activate_permanent_effect()
+
+	charactersOnPayField.erase(card)
+
+func _reorder_diamonds():
+	for i in range(diamonds.size()):
+		diamonds[i].position.x = get_viewport().size.x - 24.0 - i * (CARD_WIDTH + 24.0)
+	
+func reorder_resource_cards():
+	for i in range(resourcesOnHand.size()):
+		resourcesOnHand[i].position.x = 24.0 + i * (CARD_WIDTH + 24.0)
+
+
+func _on_visibility_changed() -> void:
+	for card in resourcesOnHand:
+		card.visible = visible
+	
+	for card in charactersOnPayField:
+		card.visible = visible
+
+	for card in diamonds:
+		card.visible = visible
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+func concat(arr: Array) -> String:
+	var result = ""
+	for num in arr:
+		result += str(num)
+	return result
