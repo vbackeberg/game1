494b18d51bc40728f67deb240c74306094f0f355
Valerian Backeberg <valerian@backebergs.de>
2026-06-20 08:33:06 +0200

    wip

---
+++ test/touch-drag/test.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+var is_dragging := false
+
+func _on_area_3d_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true:
+			print("pressed mouse")
+	pass # Replace with function body.
