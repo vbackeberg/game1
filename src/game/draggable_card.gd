1cf02a29ffacf1754523414b173812a8e7cb6e07
Valerian <valerian@backebergs.de>
2026-06-27 08:05:16 +0200

    Draggable Card: use mouse entered/exited signals from collision shape. Size collision shape to sprite size. Create draggable card test scene

--- src/game/draggable_card.gd
+++ src/game/draggable_card.gd
@@ -3,27 +3,7 @@ extends Node2D
 var is_dragging := false
 var mouse_offset: Vector2
 const delay := 1
-
-#TODO: collision shape determines front back sizes and detects drag input
-
-@onready
-var shape: RectangleShape2D = $CollisionShape2D.shape
-
-#func _ready() -> void:
-	#var size := $CollisionShape2D.shape
-
-	#front.centered = true
-	#back.centered = true
-#
-	#front.scale = Vector2(
-		#size.x / front.texture.get_width(),
-		#size.y / front.texture.get_height()
-	#)
-#
-	#back.scale = Vector2(
-		#size.x / back.texture.get_width(),
-		#size.y / back.texture.get_height()
-	#)
+var mouse_inside := false
 
 func _physics_process(delta: float) -> void:
 	if is_dragging:
@@ -39,8 +19,15 @@ func handle_mouse_button(event: InputEventMouseButton) -> void:
 		return
 
 	if event.pressed:
-		if $CollisionShape2D.get_rect().has_point(to_local(event.position)):
 			is_dragging = true
 			mouse_offset = get_global_mouse_position() - global_position
 	else:
 		is_dragging = false
+
+
+func _on_mouse_entered() -> void:
+	mouse_inside = true
+
+
+func _on_mouse_exited() -> void:
+	mouse_inside = false
