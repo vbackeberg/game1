21d13c7d24d24cb27599d1c34b027dc25b7d9b76
Valerian Backeberg <valerian@backebergs.de>
2026-07-04 07:22:36 +0200

    draggable card: snap with physics process

--- src/game/draggable_card.gd
+++ src/game/draggable_card.gd
@@ -6,10 +6,21 @@ var is_dragging := false
 var mouse_offset: Vector2
 const delay := 1
 
+var is_snapping := false
+var snap_position: Vector2
+const snap_delay := 10
+
 func _physics_process(delta: float) -> void:
 	if is_dragging:
+		is_snapping = false
 		var tween = get_tree().create_tween()
 		tween.tween_property(self, "position", get_global_mouse_position() - mouse_offset, delay * delta)
+	elif is_snapping:
+		if position == snap_position:
+			is_snapping = false
+		var tween = get_tree().create_tween()
+		tween.tween_property(self, "position", snap_position, snap_delay * delta)
+		
 		
 func _on_input_event(__: Node, event: InputEvent, ___: int) -> void:
 	if event is InputEventMouseButton:
@@ -27,8 +38,8 @@ func handle_mouse_button(event: InputEventMouseButton) -> void:
 		dropped.emit()
 
 func snap_to(target_position: Vector2):
-	var tween = get_tree().create_tween()
-	tween.tween_property(self, "position", target_position, delay)
+	snap_position = target_position
+	is_snapping = true
 
 # Exposes the textures from the inner scene here so they can be set when instancing this scene on characters
 @export var front_texture: Texture2D:
